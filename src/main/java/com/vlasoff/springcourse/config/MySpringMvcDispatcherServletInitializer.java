a498b4fa209ff16824e80b25ced633fc6bb37c14
mulasau <mikevdga@gmail.com>
2023-02-19 09:16:44 -0500

    First commit.

---
+++ src/main/java/com/vlasoff/springcourse/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,20 @@
+package com.vlasoff.springcourse.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
